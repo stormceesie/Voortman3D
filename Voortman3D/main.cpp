87a7fdf58afd48ccc56c89ba28196d52c438aca1
sormceesie <kegler.florent@gmail.com>
2024-07-18 21:51:36 +0200

    Setup camera

--- Voortman3D/main.cpp
+++ Voortman3D/main.cpp
@@ -5,5 +5,11 @@ namespace Voortman3D {
 		this->title = L"Voortman3D";
 		this->name = L"Voortman3D";
 		this->icon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+
+		camera.type = Camera::CameraType::lookat;
+		camera.flipY = true;
+		camera.setPosition(glm::vec3(0.0f, -0.1f, -1.0f));
+		camera.setRotation(glm::vec3(0.0f, 45.0f, 0.0f));
+		camera.setPerspective(60.0f, static_cast<float>(width) / static_cast<float>(height), 0.1f, 256.0f);
 	}
 }
